dcb5dd0f0ed33e2f8be5d3d63bae6bc1e031d697
Jason Long <jason.long77@hotmail.com>
2020-09-28 23:18:08 +0000

    Created route for the POST request and generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+  let randomString = Math.random().toString(36).substring(2,8);
+  return randomString;
+};
 
 const urlDatabase = {
   'b2xVn2': "http://www.lighthouselabs.ca",
@@ -21,6 +28,15 @@ app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+})
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
