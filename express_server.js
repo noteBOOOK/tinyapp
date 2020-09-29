566540fc82c998756358c02a36a3edd259f1a87d
Jason Long <jason.long77@hotmail.com>
2020-09-29 22:19:34 +0000

    Added cookie parser, added a login form in _header partial and created login route in express server

--- express_server.js
+++ express_server.js
@@ -66,6 +66,11 @@ app.post('/urls/:shortURL', (req, res) => {
   res.redirect('/urls');
 })
 
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+})
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
